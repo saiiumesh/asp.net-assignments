aa273007f1100c0b4e953d2fc2973a12faf61b85
umesh <56065659+saiiumesh@users.noreply.github.com>
2022-05-02 11:45:41 +0530

    hands on assignment 3

---
+++ hands on assignment 3/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace Assignment3.Models
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(): base("name=conn")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
